62d7674bbbf67b967bb8748e6f621ba1fdbf86df
Michael Stuhr <m.stuhr@novagraphix.de>
2017-08-15 13:03:34 +0200

    pimped

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 'use srict';
+
 document.addEventListener('DOMContentLoaded', function() {
 	var lang = 'de';
 	var variant = 'short';
@@ -8,6 +9,45 @@ document.addEventListener('DOMContentLoaded', function() {
 	function trace(o) {
 		console.log(o);
 	}
+
+	/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */ 
+	function hasClass(el, className) {
+	    return el.classList ? el.classList.contains(className) : new RegExp('\\b'+ className+'\\b').test(el.className);
+	}
+	
+	function addClass(el, className) {
+	    if (el.classList) el.classList.add(className);
+	    else if (!hasClass(el, className)) el.className += ' ' + className;
+	}
+	
+	function removeClass(el, className) {
+	    if (el.classList) el.classList.remove(className);
+	    else el.className = el.className.replace(new RegExp('\\b'+ className+'\\b', 'g'), '');
+	}
+
+	function getRandomInt(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min +1)) + min; 
+	};
+
+	// helper for enabling IE 8 event bindings
+	function addEvent(el, type, handler) {
+	    if (el.attachEvent) el.attachEvent('on'+type, handler); else el.addEventListener(type, handler);
+	}
+	// live binding helper with CSS selector
+	function live(selector, event, callback, context) {
+	    addEvent(context || document, event, function(e) {
+	        var qs = (context || document).querySelectorAll(selector);
+	        if (qs) {
+	            var el = e.target || e.srcElement, index = -1;
+	            while (el && ((index = Array.prototype.indexOf.call(qs, el)) === -1)) el = el.parentElement;
+	            if (index > -1) callback.call(el, e);
+	        }
+	    });
+	}
+
+	/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */ 
 
 	function loadJSON( file, callback ) {   
 	 	var xobj = new XMLHttpRequest();
@@ -21,16 +61,6 @@ document.addEventListener('DOMContentLoaded', function() {
 	 	xobj.send(null);
 	};
 
-	function getRandomInt(min, max) {
-		min = Math.ceil(min);
-		max = Math.floor(max);
-		return Math.floor(Math.random() * (max - min +1)) + min; 
-	};
-
-	// async, so wordlist won't be defined until fully loaded, keep in mind!
-	loadJSON('data/wordlist.json', function(response) {
-		wordlist = JSON.parse(response);
-	});
 
 	function pwgen() {
 	   var pw = [];
@@ -59,15 +89,22 @@ document.addEventListener('DOMContentLoaded', function() {
 	};
 
 	function init() {
-		document.querySelectorAll( 'form' )[0].reset()
-		// document.getElementById( 'result' ).value = '';
 
+		// async, so wordlist won't be defined until fully loaded, keep in mind!
+		loadJSON('data/wordlist.json', function(response) {
+			wordlist = JSON.parse(response);
+		});
+
+
+		document.querySelectorAll( 'form' )[0].reset();
 
 		document.getElementById('Generate').onclick = function(e){
 	  		pwgen();
 		};
-		document.getElementsByClassName('video_box')[0].addEventListener('click', function(e){
+
+		/*document.getElementsByClassName('video_box')[0].addEventListener('click', function(e){
 			var el = e.target; // placeholder
+			addClass(el.parentNode, 'active');
 			var src = el.getAttribute('data-src') + '/?&autoplay=1';
 			el.style.display = 'none';
 			var iframe = el.parentNode.getElementsByTagName('iframe')[0];
@@ -75,7 +112,44 @@ document.addEventListener('DOMContentLoaded', function() {
 				// iframe.click()
 			};
 			iframe.setAttribute('src', src);
+		});*/
+		live('.video_box .placeholder', 'click', function(ev){
+			addClass(this.parentNode, 'active');
+			var src = this.getAttribute('data-src') + '/?&autoplay=1';
+			addClass(this, 'hidden');
+			var iframe = this.parentNode.getElementsByTagName('iframe')[0];
+			iframe.onload = function(ev) {
+				// iframe.click()
+			};
+			iframe.setAttribute('src', src);
 		});
+
+		live('.video_box .close', 'click', function(ev){
+			ev.preventDefault();
+	    	// prevent the event fro bubbling up
+	    	ev.stopPropagation();
+	    	removeClass(this.parentNode, 'active');
+	    	removeClass(document.querySelectorAll('.video_box .placeholder')[0], 'hidden');
+	    	var iframe = this.parentNode.getElementsByTagName('iframe')[0];
+			iframe.setAttribute('src', '');
+		});
+
+		var tooltip = new Drooltip({
+			'element' : '.tooltip',
+			'trigger' : 'hover',
+			// 'position' : 'top',
+			'background' : 'purple',
+			'color' : '#fff',
+			'animation': 'material',
+			// 'content' : null,
+			'callback' : null
+  		});
+
+		if( location.host === 'localhost' ) {
+			var livereload = document.createElement('script');
+			livereload.setAttribute('src','http://localhost:35729/livereload.js?snipver=1');
+			document.head.appendChild(livereload);
+		}
 	}
 
 	init();
